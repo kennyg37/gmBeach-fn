fdbd0302fd256c9669a19b55a38fe04a077b91ea
kennyg37 <k.ganza@alustudent.com>
2025-06-25 06:35:17 +0200

    finishing up

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -11,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  build: {
+    outDir: 'dist' // Vercel expects this for static output
+  },
+});
